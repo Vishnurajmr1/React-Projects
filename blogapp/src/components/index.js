3dce57fb529574e986a4a4b4c2c775a735881484
Vishnurajmr1 <vishnurajmr1@gmail.com>
2024-09-12 20:00:45 +0530

    feat:implemented tinyeditor

--- blogapp/src/components/index.js
+++ blogapp/src/components/index.js
@@ -1,3 +1,25 @@
 import Protected from "./AuthLayout";
+import Button from "./Button";
+import Input from "./Input";
+import Select from "./Select";
+import Logo from "./Logo";
+import PostCard from "./PostCard";
+import Container from "./container/Container";
+import Footer from "./footer/Footer";
+import Header from "./header/Header";
+import LogoutBtn from "./header/LogoutBtn";
+import RTE from "./RTE";
 
-export { Protected };
+export {
+  Protected,
+  Button,
+  Input,
+  Select,
+  Logo,
+  PostCard,
+  Container,
+  Footer,
+  Header,
+  LogoutBtn,
+  RTE
+};
