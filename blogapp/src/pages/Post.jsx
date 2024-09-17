7ce75c10fe0a232335f6d49a30e2407dcd5fc088
Vishnurajmr1 <vishnurajmr1@gmail.com>
2024-09-18 00:09:47 +0530

    feat:login component complete

--- blogapp/src/pages/Post.jsx
+++ blogapp/src/pages/Post.jsx
@@ -1,9 +1,46 @@
-import React from 'react'
-
+import React, { useEffect, useState } from "react";
+import { Link, useNavigate, useParams } from "react-router-dom";
+import appwriteService from "../appwrite/config.js";
+import { Button, Container } from "../components/index.js";
+import parse from "html-react-parser";
+import { useSelector } from "react-redux";
 const Post = () => {
-  return (
-    <div>Post</div>
-  )
-}
+  const [post, setPost] = useState(null);
+  const { slug } = useParams();
+  const navigate = useNavigate();
+  const userData = useSelector((state) => state.auth.userData);
+  const isAuthor = post && userData ? post.userId === userData.$id : false;
+  useEffect(() => {
+    if (slug) {
+      appwriteService.getPost(slug).then((post) => {
+        if (post) {
+          setPost(post);
+        } else {
+          navigate("/");
+        }
+      });
+    }
+  }, [slug, navigate]);
+  const deletePost = () => {
+    appwriteService.deletePost(post.$id).then((status) => {
+      if (status) {
+        appwriteService.deleteFile(post.featuredImage);
+        navigate("/");
+      }
+    });
+  };
+  return post ? (
+    <div className="py-8">
+      <Container>
+        <div className="w-full flex justify-center mb-4 relative border rounded-xl p-2">
+          <img
+            src={appwriteService.getFilePreview(post.featuredImage)}
+            alt={post.title}
+          />
+        </div>
+      </Container>
+    </div>
+  ) : null;
+};
 
-export default Post
+export default Post;
